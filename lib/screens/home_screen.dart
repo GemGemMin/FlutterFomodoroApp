395f7e930c1b0d7ec27d8cde8671b70c85e34749
GemGemMin <kkm32543@khu.ac.kr>
2024-07-18 22:04:01 +0900

    add StopButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,13 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
   late Timer timer; // late modifier는 이 property를 당장 초기화하지 않아도 된다는 것을 뜻함.
+  int totalPomodoros = 0;
+  bool isRunning = false;
 
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds--;
-    });
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros++;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds--;
+      });
+    }
   }
 
   void onStartPressed() {
@@ -25,6 +37,31 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onStopPressed() {
+    setState(() {
+      totalPomodoros = 0;
+      totalSeconds = twentyFiveMinutes;
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds).toString().split('.').first.substring(
+        2,
+        7); // toString으로 문자열 변경, split으로 초의 소수점 부분 분리, first로 분리된 것중 시:분:초만 남기고, substring으로 시간 부분 날림.
+    return duration;
   }
 
   @override
@@ -38,7 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                format(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -50,11 +87,27 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 2, // flex로 고정적인 것이 아닌 화면 내에서 차지하는 비율을 설정할 수 있다.
             child: Center(
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.play_circle_outline),
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(isRunning
+                        ? Icons.pause_circle_outline
+                        : Icons.play_circle_outline),
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                  ),
+                  if (!isRunning && totalSeconds < 1500)
+                    IconButton(
+                      onPressed: onStopPressed,
+                      icon: const Icon(
+                        Icons.stop_circle_outlined,
+                      ),
+                      iconSize: 120,
+                      color: Theme.of(context).cardColor,
+                    )
+                ],
               ),
             ),
           ),
@@ -82,7 +135,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          '0',
+                          '$totalPomodoros',
                           style: TextStyle(
                             fontSize: 58,
                             color: Theme.of(context)
